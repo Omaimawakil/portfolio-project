1bd9001462464e4e6178a387bfae1ce18b8bfbfe
Omaimawakil <omaimawakil38@gmail.com>
2026-05-30 20:21:16 +0530

    Updated project code

--- client/script.js
+++ client/script.js
@@ -1,17 +1,23 @@
 fetch('http://localhost:5000/projects')
-.then(response => response.json())
-.then(data => {
+  .then(response => response.json())
+  .then(data => {
 
-  const projectDiv = document.getElementById('projects');
+    const projectDiv = document.getElementById('projects');
 
-  data.forEach(project => {
+    data.forEach(project => {
 
-    projectDiv.innerHTML += `
-      <div class="project">
-        <h2>${project.title}</h2>
-        <p>${project.description}</p>
-      </div>
-    `;
-  });
+      projectDiv.innerHTML += `
+        <div class="project">
+          <h2>${project.title}</h2>
+          <p>${project.description}</p>
+          ${
+            project.github
+              ? `<a href="${project.github}" target="_blank">View on GitHub</a>`
+              : `<p><em>College Project</em></p>`
+          }
+        </div>
+      `;
+    });
 
-});
+  })
+  .catch(error => console.error(error));
--- server/server.js
+++ server/server.js
@@ -1,25 +1,34 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const Project=require('./models/Project');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://OmaimaUsingMongo:Omaima%40UsesMongo@cluster0.mpggtmm.mongodb.net/portfolioDB?retryWrites=true&w=majority&appName=Cluster0')
+/*mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+.catch(err => console.log(err));*/
+console.log(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB Connected");
+  })
+  .catch(err => {
+    console.error("❌ MongoDB Error:");
+    console.error(err);
+  });
 
-const projects = [
-  {
-    title: "Portfolio Website",
-    description: "My personal portfolio"
-  }
-];
-
-app.get('/projects', (req, res) => {
+app.get('/projects', async (req, res) => {
+  const projects = await Project.find();
   res.json(projects);
+});
+
+app.post('/projects', async (req, res) => {
+  const project = await Project.create(req.body);
+  res.json(project);
 });
 
 const PORT = 5000;
